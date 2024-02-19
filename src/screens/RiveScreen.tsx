fb6438dfd4650ee1b793eb62cc09e4212d21516d
Agata Kosior <agata@Agatas-MacBook-Pro.local>
2024-02-19 19:09:00 +0100

    feat: setup rive

--- src/screens/RiveScreen.tsx
+++ src/screens/RiveScreen.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import Rive from 'rive-react-native';
 
 export const RiveScreen = () => {
   return (
-    <View>
-      <Text>Rive</Text>
-    </View>
+    <Rive
+      url="https://public.rive.app/community/runtime-files/2195-4346-avatar-pack-use-case.riv"
+      artboardName="Avatar 1"
+      stateMachineName="avatar"
+      style={{width: 400, height: 400}}
+    />
   );
 };
